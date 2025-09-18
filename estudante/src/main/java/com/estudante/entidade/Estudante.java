6ed9d3fb3e456a96019ef6d13c247d89b72c3ab3
Killian D.B. <115996000+KillianDB@users.noreply.github.com>
2025-09-18 00:43:33 +0000

    feat: micros

---
+++ estudante/src/main/java/com/estudante/entidade/Estudante.java
@@ -0,0 +1,13 @@
+package com.estudante.entidade;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Estudante {
+    private String id;
+    private String name;
+}
